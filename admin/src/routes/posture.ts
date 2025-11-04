5c18f57147d618fcd623a807de6bd580736dc720
Haseeb Alvi <haseebalvi91@gmail.com>
2025-11-04 18:39:45 +0500

    fixes

--- admin/src/routes/posture.ts
+++ admin/src/routes/posture.ts
@@ -5,7 +5,7 @@ import { ensureAuth } from "../middleware/keycloak";
 const router = Router();
 const controller = new PostureController();
 
-router.post("/posture-check", ensureAuth, (req, res) => controller.check(req, res));
+router.post("/posture-check",  (req, res) => controller.check(req, res));
 router.get("/posture-report", ensureAuth, (req, res) => controller.report(req, res));
 
 export default router;
