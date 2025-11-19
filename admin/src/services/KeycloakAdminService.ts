47f0a67a6e2273db902c3a030b95897f0a73354d
Haseeb Alvi <haseebalvi91@gmail.com>
2025-11-20 00:18:09 +0500

    fixes

--- admin/src/services/KeycloakAdminService.ts
+++ admin/src/services/KeycloakAdminService.ts
@@ -20,12 +20,36 @@ export class KeycloakAdminService {
     params.append("client_id", "admin-cli");
     params.append("username", this.adminUser);
     params.append("password", this.adminPass);
+
     const res = await axios.post(url, params);
     return res.data.access_token;
   }
 
+  private async getRoleId(token: string, roleName: string) {
+    const url = `${this.baseUrl}/admin/realms/${this.realm}/roles/${roleName}`;
+    const res = await axios.get(url, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    return res.data; // roleRepresentation
+  }
+
+  private async assignRoleToUser(
+    token: string,
+    userId: string,
+    roleRep: any
+  ): Promise<void> {
+
+    const url = `${this.baseUrl}/admin/realms/${this.realm}/users/${userId}/role-mappings/realm`;
+
+    await axios.post(url, [roleRep], {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+
   async createUser(email: string, tempPassword: string): Promise<string> {
     const token = await this.getAdminToken();
+
+    // Create user
     const url = `${this.baseUrl}/admin/realms/${this.realm}/users`;
     const userPayload = {
       username: email,
@@ -40,13 +64,18 @@ export class KeycloakAdminService {
         },
       ],
     };
+
     const res = await axios.post(url, userPayload, {
       headers: { Authorization: `Bearer ${token}` },
     });
 
-    // location header has ID
     const location = res.headers["location"] as string;
     const userId = location.split("/").pop() as string;
+
+    // --- NEW: assign role "z-box-user"
+    const roleRep = await this.getRoleId(token, "z-box-user");
+    await this.assignRoleToUser(token, userId, roleRep);
+
     return userId;
   }
 }
